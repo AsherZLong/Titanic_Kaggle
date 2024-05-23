08be074f52ba8e4d4b98e039ba15bb375a178aea
Asher Long <asherlong1@gmail.com>
2024-05-23 17:14:50 +0100

    sorted previous issue with f-string, now playing with imputation but not receiving different MAE for different methods?

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
 
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
@@ -13,35 +14,25 @@ useful_features = ['Age', 'Pclass', 'Fare', 'SibSp', 'Parch']
 X = train_data[useful_features]
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
+my_imputer = SimpleImputer(strategy='constant', fill_value = 0)
+i_train_X = pd.DataFrame(my_imputer.fit_transform(train_X))
+i_val_X = pd.DataFrame(my_imputer.transform(val_X))
 
-def get_mae(test_val, train_X, val_X, train_y, val_y):
-    forest_model = RandomForestRegressor(n_estimators=test_val, random_state=1)
-    forest_model.fit(train_X, train_y)
-    predictions = forest_model.predict(val_X)
+
+def get_mae(i_train_X, i_val_X, train_y, val_y):
+    forest_model = RandomForestRegressor(n_estimators=10, max_depth=20,
+                                         random_state=1)
+    forest_model.fit(i_train_X, train_y)
+    predictions = forest_model.predict(i_val_X)
     return mean_absolute_error(val_y, predictions)
 
 
-for estimators in [10, 50, 100, 200, 500, 1000]:
-    my_mae = get_mae(estimators, train_X, val_X, train_y, val_y)
-    print('for n_estiamtors: %d \t\t MAE: %d ' % (estimators, my_mae))
+my_mae = get_mae(train_X, val_X, train_y, val_y)
+
+print(f'Using mean method:, {my_mae}')
 
 
 # EDA
 # training data vs survivor data shows survivros were: on average slightly younger (28.3 vs 29.7); they were higher class (avg 1.95 vs 2.31);
 # had less Siblings/Spouses (0.474 vs 0.523); had more Parents/Children (0.465 vs 0.382) and paid more (48.4 vs 32.2).
 # There are 177 empty age entries in train data.
-
-'''y = train_data["Survived"]
-
-features = ["Pclass", "Sex", "SibSp", "Parch", "Age"]
-X = pd.get_dummies(train_data[features])
-X_test = pd.get_dummies(test_data[features])
-# for i in range(10):
-model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-model.fit(X, y)
-predictions = model.predict(X_test)
-print(mean_absolute_error(y,predictions))
-
-output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-output.to_csv('submission.csv', index=False)
-print("Your submission was successfully saved!")'''
